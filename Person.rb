15dab2b84468cfffdc423ce549afe920c73d981c
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-09-27 00:31:09 +0500

    Update Person.rb
    Person filed updated to remove linter errors

--- Person.rb
+++ Person.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
-# created a base Class Person
 class Person
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'unknown', parent_permission: true)
+    @id = Random.rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -11,17 +9,13 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def can_use_services
-    if is_of_age && @parent_permission
-      true
-    else
-      false
-    end
+  def can_use_services?
+    is_of_age? || @parent_permission
   end
 
   private
 
-  def is_of_age
-    age >= 18
+  def is_of_age? # rubocop:disable Naming/PredicateName
+    @age >= 18
   end
 end
