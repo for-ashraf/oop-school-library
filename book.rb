973a4af8ff6317ceafa92022b03b529aa6b18721
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-09-27 21:27:32 +0500

    Creation of Class
    - Created the Rental Class
    - Corrected Book class

--- book.rb
+++ book.rb
@@ -1,7 +1,7 @@
 class Book
   def initialize(title, author)
     @title = title
-    @author= author
+    @author = author
   end
 
   attr_accessor :title, author
---
+++ rental.rb
@@ -0,0 +1,8 @@
+class Rental
+  def initialize(date)
+    @date = date
+    @author = author
+  end
+
+  attr_accessor :date
+end
